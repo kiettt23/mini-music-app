bdebb277e3be6733c94be4ca9b0b382f238be618
kiettt23 <kiettt23@gmail.com>
2025-09-22 22:24:23 +0700

    feat(app): setup App.jsx with MusicPlayerProvider and main layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { MusicPlayerProvider } from "./contexts/MusicPlayerContext";
+import TrackList from "./components/TrackList";
+import Controller from "./components/Controller";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <MusicPlayerProvider>
+      <div className="container">
+        <h1>Mini Music App</h1>
+        <TrackList />
+        <Controller />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </MusicPlayerProvider>
+  );
+};
 
-export default App
+export default App;
